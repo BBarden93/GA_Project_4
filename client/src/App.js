d7580290631dcb6c0c3b61a7bdc645451e91bc8a
Brittany Barden <bbarden93@yahoo.com>
2018-04-09 14:11:47 -0700

    render home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Link, Route, Switch, Redirect} from 'react-router-dom';
+import httpClient from './httpClient.js';
+import Home from './views/Home.js';
+import LogIn from './views/LogIn.js';
+import SignUp from './views/SignUp.js';
 
 class App extends Component {
+
+  state = {
+    currentUser: httpClient.getCurrentUser() 
+  }
+
+  onLoginSuccess(user){
+    this.setState({currentUser: user})
+  }
+
   render() {
+    const {currentUser} = this.state 
+    
     return (
+
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          {currentUser 
+            ? (
+              <div>{currentUser.name}</div>
+            )
+            : null
+          }
+        <div>
+          <Link to="/">Home</Link>
+          {/* <Link to="/vip">VIP</Link> */}
+          <Link to="/login">Log In</Link>
+          <Link to="/signup">Sign Up</Link>
+          {/* <Link to="/logout">Log Out</Link> */}
+        </div>
+
+        <Switch>
+            <Route path="/login" render={(routeProps) => {
+                return <LogIn {...routeProps} onLoginSuccess={this.onLoginSuccess.bind(this)}/>
+            }} />
+            
+            <Route path="/signup" render={(routeProps) => {
+                return <SignUp {...routeProps} onSignupSuccess={this.onLoginSuccess.bind(this)}/>
+            }} />
+            
+            {/* <Route path="/logout" render={() => {
+                httpClient.logOut()
+                setTimeout(() => { this.setState({currentUser: null}) })
+                return <Redirect to="/login" />
+            }} />
+
+            <Route path="/vip" render={() => {
+                return currentUser 
+                ? <h1>VIP, Welcome</h1>
+                : <Redirect to="/login" />
+            }} /> */}
+
+            <Route path="/" component={Home} />
+        </Switch>
       </div>
     );
   }
