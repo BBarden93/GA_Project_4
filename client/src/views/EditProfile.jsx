91891075bdd5dc78e969492858bc0e384604a8ec
Brittany Barden <bbarden93@yahoo.com>
2018-04-15 19:52:03 -0700

    css

--- client/src/views/EditProfile.jsx
+++ client/src/views/EditProfile.jsx
@@ -49,6 +49,19 @@ class EditProfile extends React.Component {
 							<input type="password" placeholder="Password" name="password" value={password} />
 							<button>Update Profile</button>
 						</form>
+						{/* <Form inline>
+							<FormGroup>
+							<Label for="exampleEmail" hidden>Email</Label>
+							<Input type="text" name="email" id="exampleEmail" placeholder="Email" value={email} />
+							</FormGroup>
+							{' '}
+							<FormGroup>
+							<Label for="examplePassword" hidden>Password</Label>
+							<Input type="password" name="password" id="examplePassword" placeholder="Password" />
+							</FormGroup>
+							{' '}
+							<Button>Submit</Button>
+						</Form> */}
 					</div>
 				</div>
             </div>
--- client/src/views/Questions.jsx
+++ client/src/views/Questions.jsx
@@ -19,14 +19,14 @@ class Questions extends React.Component {
         const {questions} = this.state 
         console.log(questions)
         return (
-            <div className="Questions" style={{textAlign: 'center'}}>
+            <div className="Questions" >
                 <h1>Questions: </h1>
-                <Link to="/questions/new">Ask a Question</Link>
+                <Link className="askQ" to="/questions/new">Ask a Question</Link>
                 <ul> 
                 {questions.map((q) => {
                     return (
                         <div key={q._id}  className="questionLink">
-                            <Link to={`/questions/${q._id}`}>{q.body} - {q.user.name}</Link><br />
+                            <Link  className="qLink"to={`/questions/${q._id}`}>{q.body} - {q.user.name}</Link><br />
                         </div>
                     )
                 })}  
