76240651dcdc8f63353452ef5955c183a15224ed
Brittany Barden <bbarden93@yahoo.com>
2018-05-01 17:43:30 -0700

    change home photo

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -9,8 +9,11 @@ const Home = (props) => {
             <h3>Ask a question. Get an answer... Or a bunch!</h3>
             </div>
             <div className="HomeImage">
-                <img src="https://wallpaperscraft.com/image/desert_road_marking_4896_3840x1200.jpg" alt="Desert Road" />
-                {/* https://i.imgur.com/VT2GAz3.jpg */}
+                <img src="https://i.imgur.com/k1cdgcn.jpg" alt="Desert Road" />
+                {/* 
+                https://i.imgur.com/k1cdgcn.jpg
+                https://wallpaperscraft.com/image/desert_road_marking_4896_3840x1200.jpg
+                */}
             </div>
         </div>
     )
